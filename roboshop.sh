e6e425ef7da7cac2210531cb839c8a37573d9676
meenakshi <meena@gmail.com>
2024-12-22 19:10:34 +0530

    roles

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
 SG_ID=sg-02cfa8ece2d41bfd1 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z03017021B9N6TSSRTZCC # replace your zone ID
+ZONE_ID=Z03048852B5HYPKNUYKB1 # replace your zone ID
 DOMAIN_NAME="daws80.online"
 
 for i in "${INSTANCES[@]}"
