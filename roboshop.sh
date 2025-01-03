f9f4de47a599d673f171c5c6f6e50c7cd3f410ff
meenakshi <meena@gmail.com>
2025-01-03 10:41:56 +0530

    commit

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
 SG_ID=sg-02cfa8ece2d41bfd1 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z00704861DH7NQCLIZEIU
+ZONE_ID=Z04084741QTKF93ZEAB9V
 DOMAIN_NAME="daws80.online"
 
 for i in "${INSTANCES[@]}"
